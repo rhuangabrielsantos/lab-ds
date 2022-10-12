47a073d2de2c6568dbb3e10d2d2ec32c7219290e
Rhuan Gabriel <rhuan.santos@db1.com.br>
2022-10-11 21:20:24 -0300

    :lipstick: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+
+export interface ButtonProps {
+  children: ReactNode;
+  asChild?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  asChild,
+}) => {
+  const Component = asChild ? Slot : 'button';
+
+  return (
+    <Component className="py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white">
+      {children}
+    </Component>
+  );
+};
